9b3c401bfd8d8565759ca592aeec0f022107541b
Zachary Berardo <zachary@zachnthebox.com>
2018-06-20 22:41:52 -0400

    fix: markdown precompiler in correct order

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 'use strict';
-var path = require('path');
+const path = require('path');
 
 module.exports = {
   name: 'ember-nrg-ui',
@@ -11,6 +11,7 @@ module.exports = {
 
   included: function(app) {
     this._super.included.apply(this, arguments);
+
     const jsSource = 'node_modules/semantic-ui-calendar/dist';
     app.import({
       development: path.join(jsSource, 'calendar.js'),
@@ -20,8 +21,10 @@ module.exports = {
 
   setupPreprocessorRegistry(type, registry) {
     if (type === 'parent') {
-      const TemplateCompiler = require('./lib/preprocessors/markdown-template-compiler');
-      registry.add('template', new TemplateCompiler());
+      const MarkdownTemplateCompiler = require('./lib/preprocessors/markdown-template-compiler');
+      const plugin = new MarkdownTemplateCompiler();
+      registry.instantiatedPlugins.push(plugin);
+      registry.registeredForType('template').unshift(plugin);
     }
   },
 };
