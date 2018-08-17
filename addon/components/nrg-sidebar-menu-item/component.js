181506070697e9001c5a0935bd82b85323af48b1
Zachary Berardo <zachary@zachnthebox.com>
2018-08-17 11:35:41 -0400

    fix(sidebar-menu): group headers now align correctly with other items (#64)

--- addon/components/nrg-sidebar-menu-item/component.js
+++ addon/components/nrg-sidebar-menu-item/component.js
@@ -3,6 +3,9 @@ import layout from './template';
 import {
   readOnly
 } from '@ember/object/computed';
+import {
+  computed
+} from '@ember/object';
 
 export default Component.extend({
   layout,
@@ -10,4 +13,10 @@ export default Component.extend({
   tagName: '',
 
   isVisible: readOnly('item.isShownInSidebar'),
+
+  isParentGroup: false,
+
+  _linkToItemClass: computed('isParentGroup', function() {
+    return this.get('isParentGroup') ? 'parent-route': 'item';
+  }),
 });
