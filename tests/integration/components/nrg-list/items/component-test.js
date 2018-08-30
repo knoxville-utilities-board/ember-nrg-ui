7ac96cc77602ad0cb88010d23ee0fdd34af33b51
MKA07503 <Michael.Kaufman@kub.org>
2018-08-30 10:39:29 -0400

    Add test for if isSelectable should return false

--- tests/integration/components/nrg-list/items/component-test.js
+++ tests/integration/components/nrg-list/items/component-test.js
@@ -8,6 +8,9 @@ import {
 import {
   render
 } from '@ember/test-helpers';
+import {
+  A,
+} from '@ember/array';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | nrg-list/items', function(hooks) {
@@ -18,14 +21,36 @@ module('Integration | Component | nrg-list/items', function(hooks) {
       label: 'label',
     };
     this.items = [item];
+    this.selected = A([]);
     this.selectAction = function(selectedItem) {
       assert.equal(selectedItem, item);
     };
     this.isSelectable = function() {
       return true;
     }
-    await render(hbs `{{nrg-list/items isSelectable=isSelectable selectionType='single' items=items itemClicked=(action selectAction)}}`);
+    await render(hbs `{{nrg-list/items isSelectable=isSelectable selectionType='single' selected=selected items=items itemClicked=(action selectAction)}}`);
     this.$('.item').click();
+    assert.equal(this.selected.length, 1);
+  });
+
+  test('can not select if isSelectable returns false', async function(assert) {
+    const item = {
+      label: 'label',
+    };
+    this.selected = A([]);
+    this.items = [item];
+    this.selectAction = function(selectedItem) {
+      assert.equal(selectedItem, item);
+    };
+
+    this.isSelectable = function(item) {
+      if (item.label == 'label') {
+        return false;
+      }
+    }
+    await render(hbs `{{nrg-list/items isSelectable=isSelectable selectionType='multiple' selected=selected items=items itemClicked=(action selectAction)}}`);
+    this.$('.item').click();
+    assert.equal(this.selected.length, 0);
   });
 
 });
