3fcd306f928915ac57c45dd78374ddc1f43f0273
MKA07503 <Michael.Kaufman@kub.org>
2018-09-04 14:26:33 -0400

    fix: Do not use pointer cursor styling if not a selectable item

---
+++ addon/helpers/list-item-selectable.js
@@ -0,0 +1,9 @@
+import {
+  helper
+} from '@ember/component/helper';
+
+export function listItemSelectable([item, isSelectable]) {
+  return !isSelectable(item);
+}
+
+export default helper(listItemSelectable);
