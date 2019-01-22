3f57a7527a2a0b950d53699bb059849756d4af1f
Zachary Berardo <zachary@zachnthebox.com>
2019-01-22 16:18:42 -0500

    fix(sidebar): route isShownInSidebar properly respected (#115)
    * fix(sidebar): route isShownInSidebar properly respected
    
    * feat: add tests for sidebar

--- addon/components/nrg-sidebar-menu/component.js
+++ addon/components/nrg-sidebar-menu/component.js
@@ -3,6 +3,10 @@ import {
 } from '@ember/service';
 import Component from '@ember/component';
 import layout from './template';
+import {
+  filterBy,
+  notEmpty
+} from '@ember/object/computed';
 
 export default Component.extend({
   layout,
@@ -12,6 +16,12 @@ export default Component.extend({
   classNames: ['ui', 'vertical', 'left', 'menu', 'sidebar-menu'],
 
   appReloadLocation: '/',
+
+  visibleMenuItems: filterBy('sidebarMenuManager.menuItems', 'isShownInSidebar', true),
+
+  visibleFooterItems: filterBy('sidebarMenuManager.footerMenuItems', 'isShownInSidebar', true),
+
+  hasVisibleFooterItems: notEmpty('visibleFooterItems'),
 
   actions: {
     clickedLink() {
