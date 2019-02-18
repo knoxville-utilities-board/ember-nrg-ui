be2779b6b5a8a6c8f248458b1ee19c4493ab5c94
jkc07100 <keith.clinard@kub.org>
2019-02-18 12:57:46 -0500

    feat: Rewrite checkbox to avoid semantic js

--- addon/components/nrg-checkbox/component.js
+++ addon/components/nrg-checkbox/component.js
@@ -1,4 +1,8 @@
 import {
+  computed,
+  observer,
+} from '@ember/object';
+import {
   alias,
   bool
 } from '@ember/object/computed';
@@ -8,19 +12,30 @@ import Validation from 'ember-nrg-ui/mixins/validation';
 
 export default Component.extend(Validation, {
   layout,
-  tagName: '',
+  classNames: ['ui', 'checkbox'],
+  classNameBindings: ['type'],
 
   readonly: false,
   checked: alias('value'),
   _checked: bool('checked'),
   bindValue: true,
 
-  actions: {
-    onChange(value) {
-      if (this.get('bindValue')) {
-        this.set('checked', value);
-      }
-      this.sendAction('action', value);
-    },
-  },
+  inputId: computed('elementId', function(){
+    return this.get('elementId') + '-input';
+  }),
+
+  onExternalChange: observer('checked', function(){
+    const value = this.get('checked');
+    if (this.get('bindValue')) {
+      this.set('_checked', !!value);
+    }
+  }),
+
+  onInternalChange: observer('_checked', function(){
+    const value = this.get('_checked');
+    if (this.get('bindValue')) {
+      this.set('checked', !!value);
+    }
+    this.sendAction('action', !!value);
+  }),
 });
