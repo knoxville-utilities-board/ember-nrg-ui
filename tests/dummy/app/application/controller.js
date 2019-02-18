ffe4400d9a52db3c5c57e0116827dc178ac2af64
TRL07613 <Tyler.Small@kub.org>
2019-02-18 12:40:30 -0500

    fix: remove unused controller

--- tests/dummy/app/application/controller.js
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-
-export default Controller.extend({
-  actions: {
-    clickedLink(item) {
-      this.send("clickedSidebarItem", item);
-    },
-  }
-});
