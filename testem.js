e82b946e87460c6ee7b0c0d656bd4925f719c0b4
Zachary Berardo <zachary@zachnthebox.com>
2019-03-29 15:55:54 -0400

    fix: report test results correctly

--- testem.js
+++ testem.js
@@ -2,6 +2,7 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
+  report_file: 'ember-tests.xml',
   launch_in_ci: ['Chrome'],
   launch_in_dev: ['Chrome'],
   browser_args: {
