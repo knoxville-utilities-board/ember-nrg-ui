1d9eecd3b605857fc11e03cd08284e56191305ca
Zachary Berardo <zachary@zachnthebox.com>
2019-09-25 13:22:57 -0400

    fix: tests now pass

--- tests/dummy/app/view-components/nrg-lightbox/controller.js
+++ tests/dummy/app/view-components/nrg-lightbox/controller.js
@@ -1,12 +1,15 @@
 import Controller from '@ember/controller';
 import { task, timeout } from 'ember-concurrency';
+import Ember from 'ember';
 
 export default Controller.extend({
   counter: 0,
 
   counterTask: task(function*(counter = 0) {
-    yield timeout(1000);
     this.set('counter', ++counter);
-    this.get('counterTask').perform(counter);
+    if (!Ember.testing) {
+      yield timeout(1000);
+      this.get('counterTask').perform(counter);
+    }
   }).on('init'),
 });
