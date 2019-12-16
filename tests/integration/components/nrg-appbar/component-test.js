cf32c76b2fa5ce333fde0c53166db945fc00a6b0
tsmalls93 <tsmall@smallinitiatives.com>
2019-12-16 16:39:11 -0500

    test: appbar env display functionality

--- tests/integration/components/nrg-appbar/component-test.js
+++ tests/integration/components/nrg-appbar/component-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import ENV from '../../../../config/environment';
 
 module('Integration | Component | nrg-appbar', function(hooks) {
   setupRenderingTest(hooks);
@@ -14,6 +15,32 @@ module('Integration | Component | nrg-appbar', function(hooks) {
         .text()
         .trim(),
       'test title'
+    );
+  });
+
+  test('non-production environments display', async function(assert) {
+    ENV['ember-nrg-ui'] = {
+      productionEnvironments: [],
+    };
+    this.set('applicationSettings', {
+      localEnvironment: 'bob',
+    });
+    await render(hbs`{{nrg-appbar applicationSettings=applicationSettings title='test title'}}`);
+    assert.equal(
+      this.$('.environment-title').text().trim(), 'BOB'
+    );
+  });
+
+  test('production evironment should not display', async function(assert) {
+    ENV['ember-nrg-ui'] = {
+      productionEnvironments: ['bob'],
+    };
+    this.set('applicationSettings', {
+      localEnvironment: 'bob',
+    });
+    await render(hbs`{{nrg-appbar applicationSettings=applicationSettings title='test title'}}`);
+    assert.notEqual(
+      this.$('.environment-title').text().trim(), 'BOB'
     );
   });
 });
