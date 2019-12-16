5397a60abf1b88fd0e5c074304f3b5c90939c7ac
tsmalls93 <tsmall@smallinitiatives.com>
2019-12-16 08:52:29 -0500

    refactor: prod envs for appbar as array

--- addon/components/nrg-appbar/component.js
+++ addon/components/nrg-appbar/component.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 import ResizeMixin from 'ember-nrg-ui/mixins/resize';
 import Component from '@ember/component';
 import layout from './template';
+import { getOwner } from '@ember/application';
 
 export default Component.extend(ResizeMixin, {
   layout,
@@ -19,8 +20,9 @@ export default Component.extend(ResizeMixin, {
   environmentDisplay: computed(
     'applicationSettings.localEnvironment',
     function() {
+      const ENV = getOwner(this).resolveRegistration('config:environment');
       const environment = this.get('applicationSettings.localEnvironment');
-      if (environment && environment !== 'prod') {
+      if (environment && !ENV.productionEnvironments.includes(environment)) {
         return environment.toUpperCase();
       }
       return null;
--- config/environment.js
+++ config/environment.js
@@ -2,5 +2,8 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  return {};
+  const ENV = {
+    productionEnvironments: ['prod'],
+  };
+  return ENV;
 };
