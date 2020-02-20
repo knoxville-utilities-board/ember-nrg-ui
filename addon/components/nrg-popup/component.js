c95d87cd5a9bab3bd8426349dd8277bd8d523ea9
jkc07100 <keith.clinard@kub.org>
2020-02-20 11:11:30 -0500

    feat: Refactor position and support inverted

--- addon/components/nrg-popup/component.js
+++ addon/components/nrg-popup/component.js
@@ -3,35 +3,14 @@ import { computed } from '@ember/object';
 import layout from './template';
 import { task, timeout } from 'ember-concurrency';
 
-const TARGET_POSITIONS = {
-  top: 'top left',
-  bottom: 'bottom left',
-  left: 'middle left',
-  right: 'middle right',
-};
-
-const POPUP_POSITIONS = {
-  top: 'bottom left',
-  bottom: 'top left',
-  left: 'middle right',
-  right: 'middle left',
-};
-
 export default Component.extend({
   layout,
   tagName: 'span',
-  position: 'top',
   isOpen: false,
   hoverTimeout: 250,
 
   target: computed(function() {
     return this.element.querySelector('.popup-anchor');
-  }),
-  targetAttachment: computed('position', function() {
-    return TARGET_POSITIONS[this.position];
-  }),
-  popupAttachment: computed('position', function() {
-    return POPUP_POSITIONS[this.position];
   }),
 
   mouseEnter() {
