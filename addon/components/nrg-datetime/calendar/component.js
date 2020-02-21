76c67647748b1f7ada7addc7cff8dc72a3a15241
Zachary Berardo <zachary@zachnthebox.com>
2020-02-21 00:26:05 -0500

    fix: update header when day changes

--- addon/components/nrg-datetime/calendar/component.js
+++ addon/components/nrg-datetime/calendar/component.js
@@ -39,6 +39,7 @@ export default Component.extend(EKMixin, EKFirstResponderOnFocusMixin, {
   },
 
   headerDisplay: computed(
+    'selectedDayIndex',
     'selectedMonthIndex',
     'selectedYearIndex',
     'isSelectingDays',
