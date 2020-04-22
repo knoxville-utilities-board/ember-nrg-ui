d657853b1fc1fd7aee08a98ed5eeb7204ee74d85
Tyler Senter <tyler.senter.427@gmail.com>
2020-04-22 16:03:19 -0400

    fix(nrg-appbar): prevent dev env flash in app bar  (#225)
    * fix: Prevent 'DEV' from displaying before title loads
    
    * fix(nrg-appbar): Show loading indicator if title is loading
    
    * fix: Remove loading indicator and default environment

--- addon/application/settings.js
+++ addon/application/settings.js
@@ -1,6 +1,4 @@
 import EmberObject from '@ember/object';
 import Evented from '@ember/object/evented';
 
-export default EmberObject.extend(Evented, {
-  localEnvironment: 'dev',
-});
+export default EmberObject.extend(Evented, {});
