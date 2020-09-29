5b970f4f679ba8904cfe623fc3a8030ad8061d0a
Tyler Senter <tyler.senter.427@gmail.com>
2020-09-29 16:34:26 -0400

    test(home-cards): Test if hidden cards are hidden (#248)

--- tests/integration/components/nrg-home-cards/component-test.js
+++ tests/integration/components/nrg-home-cards/component-test.js
@@ -1,3 +1,4 @@
+import ObjectProxy from '@ember/object/proxy';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
@@ -5,6 +6,20 @@ import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | nrg-home-cards', function(hooks) {
   setupRenderingTest(hooks);
+  hooks.beforeEach(function() {
+    const currentUser = ObjectProxy.create({
+      content: {
+        hasRole(role) {
+          return role === 'users';
+        },
+      },
+    });
+    this.owner.register('user:current', currentUser, {
+      instantiate: false,
+      singleton: true,
+    });
+    this.owner.inject('component', 'currentUser', 'user:current');
+  });
 
   test('it renders', async function(assert) {
     await render(hbs`
@@ -18,5 +33,17 @@ module('Integration | Component | nrg-home-cards', function(hooks) {
     assert.ok(this.element.textContent.trim().includes('Item 1'));
     assert.ok(this.element.textContent.trim().includes('Item 2'));
     assert.ok(this.element.textContent.trim().includes('Item 3'));
+  });
+
+  test('access restricted cards are hidden', async function(assert) {
+    await render(hbs`
+      <NrgHomeCards as |view|>
+        <view.home-card @label="I am visible" @role="users" @icon="settings" />
+        <view.home-card @label="I am not visible" @role="administrator" @icon="settings" />
+      </NrgHomeCards>
+    `);
+
+    assert.ok(this.element.textContent.trim().includes('I am visible'), 'Visible card is shown');
+    assert.notOk(this.element.textContent.trim().includes('I am invisible'), 'Hidden card is hidden');
   });
 });
