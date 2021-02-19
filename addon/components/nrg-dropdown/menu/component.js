69f7afecb2116f80f1eebfca6cc1d6d12fea2225
cmc07458 <Matt.Clinard@kub.org>
2021-02-19 12:58:46 -0500

    fix: Add ability to make submenus scrollable

--- addon/components/nrg-dropdown/menu/component.js
+++ addon/components/nrg-dropdown/menu/component.js
@@ -1,10 +1,18 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import layout from './template';
 
 export default Component.extend({
   layout,
   tagName: '',
   subMenu: false,
+  scrollable: false,
+  scrollableClass: computed('scrollable', function() {
+    if (this.scrollable) {
+      return 'scrollable';
+    }
+    return '';
+  }),
   _onSelect() {
     // Implemented by dropdown
   },
