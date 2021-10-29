6f973694e83d2cff5b5e57bf06577f54e75b16bc
Keith <keith.clinard@kub.org>
2021-10-29 09:59:20 -0400

    feat: Convert nrg-icon to glimmer component (#309)
    * feat: Convert nrg-icon to glimmer component
    
    * chore: Formatting

--- addon/components/nrg-icon.js
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-import layout from '../templates/components/nrg-icon';
-
-export default Component.extend({
-  layout,
-  tagName: 'i',
-  classNames: ['icon'],
-  classNameBindings: ['icon'],
-});
