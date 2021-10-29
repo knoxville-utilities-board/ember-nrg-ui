c538dda4eff151188f62f364cd8a61aa092b7eb8
KeithClinard <keith@clinard.dev>
2021-10-29 13:04:32 -0400

    feat: convert nrg-text-field/trim-input to glimmer component

--- tests/integration/components/nrg-text-field/trim-input/component-test.js
+++ tests/integration/components/nrg-text-field/trim-input/component-test.js
@@ -3,21 +3,26 @@ import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
 
-module('Integration | Component | nrg-text-field/trim-input', function(hooks) {
+module('Integration | Component | nrg-text-field/trim-input', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('trim white space', async function(assert) {
+  test('trim white space', async function (assert) {
     this.set('value', 'bob     ');
-    await render(hbs`<NrgTextField::TrimInput @value={{value}} />`);
+    await render(hbs`<NrgTextField::TrimInput @value={{value}}/>`);
     assert.dom('input').hasValue('bob');
     this.set('value', 'jim     ');
     assert.dom('input').hasValue('jim');
   });
 
-  test('trim white space from user input', async function(assert) {
+  test('trim white space from user input', async function (assert) {
+    assert.expect(1);
+    this.onChange = (value) => {
+      assert.equal(value, 'bob');
+    };
     this.set('value', '');
-    await render(hbs`<NrgTextField::TrimInput @value={{value}} />`);
+    await render(
+      hbs`<NrgTextField::TrimInput @value={{value}} @onChange={{action onChange}}/>`
+    );
     await fillIn('input', 'bob     ');
-    assert.equal(this.value, 'bob');
   });
 });
