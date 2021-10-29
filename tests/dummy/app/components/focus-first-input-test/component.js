12ca97ba7b601ea84795edf09aeb57426bc362b0
Keith <keith.clinard@kub.org>
2021-10-29 13:51:18 -0400

    feat: Convert focus-first-input mixin to modifier (#317)
    * feat: Convert focus-first-input mixin to modifier
    
    * Update tests/dummy/app/view-modifiers/route.js
    
    Co-authored-by: Tyler Senter <tyler.senter.427@gmail.com>
    
    * Update tests/integration/modifiers/focus-first-input-test.js
    
    Co-authored-by: Tyler Senter <tyler.senter.427@gmail.com>

--- tests/dummy/app/components/focus-first-input-test/component.js
+++
@@ -1,8 +0,0 @@
-//BEGIN-SNIPPET focus-first-mixin
-import Component from '@ember/component';
-import FocusFirstInputMixin from 'ember-nrg-ui/mixins/focus-first-input';
-
-export default Component.extend(FocusFirstInputMixin, {
-  classNames: ['ui', 'segment'],
-});
-//END-SNIPPET
