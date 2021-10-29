755bc4b2980b56a49aadab7f4488a3edbbc7be4c
tsmalls93 <tsmall@smallinitiatives.com>
2021-10-29 13:21:21 -0400

    feat: convert nrg-sidebar-menu-item to glimmer

--- addon/components/nrg-sidebar-menu-item/content.js
+++
@@ -1,7 +0,0 @@
-import Component from '@ember/component';
-import layout from '../../templates/components/nrg-sidebar-menu-item/content';
-
-export default Component.extend({
-  layout,
-  tagName: '',
-});
