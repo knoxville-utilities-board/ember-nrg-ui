e048174a39f9928316924e702bc8d1282f944958
Keith <keith.clinard@kub.org>
2021-10-29 13:25:55 -0400

    Update tests/integration/modifiers/focus-first-input-test.js
    Co-authored-by: Tyler Senter <tyler.senter.427@gmail.com>

--- tests/integration/modifiers/focus-first-input-test.js
+++ tests/integration/modifiers/focus-first-input-test.js
@@ -8,9 +8,12 @@ module('Integration | Modifier | focus-first-input', function (hooks) {
 
   // Replace this with your real tests.
   test('it renders', async function (assert) {
-    await render(
-      hbs`<div {{focus-first-input}}><input id="test-first-input" type="text"/><input id="test-second-input" type="text"/></div>`
-    );
+    await render(hbs`
+      <div {{focus-first-input}}>
+        <input id="test-first-input" type="text" />
+        <input id="test-second-input" type="text" />
+      </div>
+    `);
     const firstInput = document.querySelector('#test-first-input');
     const secondInput = document.querySelector('#test-second-input');
     assert.equal(document.activeElement, firstInput);
