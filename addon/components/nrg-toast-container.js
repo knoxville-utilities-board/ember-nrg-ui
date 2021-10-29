8fca14e428c4ed616b845a041e94c2b2a89addf8
Keith <keith.clinard@kub.org>
2021-10-29 10:03:01 -0400

    feat: Convert nrg-toast-container to glimmer component (#311)

--- addon/components/nrg-toast-container.js
+++
@@ -1,7 +0,0 @@
-import Component from '@ember/component';
-import layout from '../templates/components/nrg-toast-container';
-
-export default Component.extend({
-  layout,
-  classNames: ['top', 'right', 'ui', 'toast-container'],
-});
