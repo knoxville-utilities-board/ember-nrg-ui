83d9ecd8268de6c2dfd8dae82f4e6be72189141c
KeithClinard <keith@clinard.dev>
2021-11-01 15:23:40 -0400

    chore: Fix tests

--- tests/integration/components/nrg-form-container/component-test.js
+++ tests/integration/components/nrg-form-container/component-test.js
@@ -3,16 +3,17 @@ import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
 
-module('Integration | Component | nrg-form-container', function(hooks) {
+module('Integration | Component | nrg-form-container', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('action fires on submit', async function(assert) {
-    this.set('testAction', function() {
+  test('action fires on submit', async function (assert) {
+    assert.expect(1);
+    this.set('onSubmit', function () {
       assert.ok(true);
     });
 
     await render(hbs`
-      <NrgFormContainer @action={{action testAction}} as |form|>
+      <NrgFormContainer @onSubmit={{action onSubmit}} as |form|>
         <form.submit-button />
       </NrgFormContainer>
     `);
--- tests/integration/components/nrg-form-field/component-test.js
+++ tests/integration/components/nrg-form-field/component-test.js
@@ -3,10 +3,10 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | nrg-form-field', function(hooks) {
+module('Integration | Component | nrg-form-field', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('input id works', async function(assert) {
+  test('input id works', async function (assert) {
     await render(hbs`
       <NrgFormField @label="label">
         <Input @id="testId" type='text' />
@@ -18,22 +18,26 @@ module('Integration | Component | nrg-form-field', function(hooks) {
     assert.equal(forProp, 'testId');
   });
 
-  test('proper errors display', async function(assert) {
+  test('proper errors display', async function (assert) {
     await render(hbs`
       <NrgFormContainer @didValidate={{true}} as |form|>
         <form.field @errorMessage="Test Message" />
+        <form.submit-button />
       </NrgFormContainer>
     `);
+    await click('button');
 
     assert.dom('.field.error .label').hasText('Test Message');
   });
 
-  test('error class shows', async function(assert) {
+  test('error class shows', async function (assert) {
     await render(hbs`
       <NrgFormContainer @didValidate={{true}} as |form|>
         <form.field @errorMessage="Test Message" />
+        <form.submit-button />
       </NrgFormContainer>
     `);
+    await click('button');
 
     assert.dom('.field').hasClass('error');
   });
