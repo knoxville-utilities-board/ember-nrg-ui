9442c97a75ec21fef11eb1517bb7c5dca832b6b1
KeithClinard <keith@clinard.dev>
2021-11-01 14:40:55 -0400

    feat: Refactor dummy app to use new validation

--- tests/dummy/app/view-components/nrg-forms/controller.js
+++ tests/dummy/app/view-components/nrg-forms/controller.js
@@ -1,16 +1,66 @@
 import Controller from '@ember/controller';
-import Validations from './validations';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import moment from 'moment';
+import validationState, { validator } from 'ember-nrg-ui/validation';
 
-export default Controller.extend(Validations, {
-  required: false,
-  disabled: false,
+const Validators = {
+  turkey: [validator('presence', true)],
+  meal: [validator('presence', true)],
+  super: [
+    validator('presence', {
+      presence: true,
+      isWarning: true,
+    }),
+  ],
+  deal: [validator('presence', true)],
+  textArea1: [validator('presence', true)],
+  checkbox1: [
+    validator('inclusion', {
+      in: [true],
+    }),
+  ],
+  textArea: [
+    validator('length', {
+      min: 0,
+      max: 5,
+    }),
+  ],
+};
 
-  maxDate: moment()
-    .add(5, 'days')
-    .toDate(),
+export default class ViewComponentsNrgFormsController extends Controller {
+  @validationState(Validators)
+  validations;
 
-  optionList: [
+  @tracked
+  required = false;
+
+  @tracked
+  disabled = false;
+
+  @tracked
+  turkey;
+
+  @tracked
+  meal;
+
+  @tracked
+  super;
+
+  @tracked
+  deal;
+
+  @tracked
+  textArea1;
+
+  @tracked
+  checkbox1;
+
+  @tracked
+  maxDate = moment().add(5, 'days').toDate();
+
+  @tracked
+  optionList = [
     {
       label: 'Item 1',
     },
@@ -26,14 +76,15 @@ export default Controller.extend(Validations, {
     {
       label: 'Item 5',
     },
-  ],
+  ];
 
-  actions: {
-    requiredToggle() {
-      this.toggleProperty('required');
-    },
-    disabledToggle() {
-      this.toggleProperty('disabled');
-    },
-  },
-});
+  @action
+  requiredToggle() {
+    this.required = !this.required;
+  }
+
+  @action
+  disabledToggle() {
+    this.disabled = !this.disabled;
+  }
+}
