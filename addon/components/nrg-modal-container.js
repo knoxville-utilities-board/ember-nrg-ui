a27db3826529873756a836aa85004a8d3cfa2c69
tsmalls93 <tsmall@smallinitiatives.com>
2021-11-04 13:18:54 -0400

    feat: convert nrg-modal-container to glimmer

--- addon/components/nrg-modal-container.js
+++ addon/components/nrg-modal-container.js
@@ -1,63 +1,13 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { readOnly } from '@ember/object/computed';
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import layout from '../templates/components/nrg-modal-container';
+import { action } from '@ember/object';
+export default class Modal extends Component {
+  @service('modal') modalService;
 
-export default Component.extend({
-  layout,
-  tagName: '',
-
-  modalService: service('modal'),
-
-  openModals: readOnly('modalService.openModals'),
-
-  activeModal: readOnly('modalService.activeModal'),
-
-  hasOpenModals: readOnly('modalService.hasOpenModals'),
-
-  activeModalIsSidebar: readOnly('activeModal.sidebar'),
-
-  _containerClass: computed('hasOpenModals', 'activeModalIsSidebar', function() {
-    let appliedClass = '';
-    if (this.hasOpenModals) {
-      appliedClass += ' visible active';
-    } else {
-      appliedClass += ' hidden';
-    }
-    if (this.activeModalIsSidebar) {
-      appliedClass += ' sidebar-container';
-    }
-    return appliedClass;
-  }),
-
-  dimmerClass: computed(
-    'activeModal.sidebar',
-    'activeModal.dimmerClass',
-    'activeModal.dismissable',
-    function() {
-      const activeModal = this.activeModal;
-      if (!activeModal) {
-        return '';
-      }
-      let appliedClass = '';
-      if (activeModal.sidebar) {
-        appliedClass += ' sidebar-dimmer';
-      } else {
-        appliedClass += ' page';
-      }
-      if (!activeModal.dismissable) {
-        appliedClass += ' not-dismissable';
-      }
-      appliedClass += ' ' + activeModal.dimmerClass;
-      return appliedClass;
-    }
-  ),
-
-  onDimmerClick() {
-    if (!this.activeModal.dismissable) {
+  @action onDimmerClick() {
+    if (!this.modalService?.activeModal?.dismissable) {
       return;
     }
-    this.activeModal.onHide();
-  },
-});
+    this.modalService?.activeModal?.onHide();
+  }
+};
