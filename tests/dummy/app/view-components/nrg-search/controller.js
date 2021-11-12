f245b94c8afdc498df1fb7d0a2f360ab66879384
Keith <keith.clinard@kub.org>
2021-11-12 10:33:54 -0500

    feat: Convert nrg-search to glimmer component (#329)
    * feat: Convert nrg-search to glimmer component
    
    * chore: PR Review Suggestions
    
    Co-authored-by: Tyler Senter <tyler.senter.427@gmail.com>
    
    * chore: PR Review
    
    * fix: Suggestion
    
    Co-authored-by: Tyler Senter <tyler.senter.427@gmail.com>

--- tests/dummy/app/view-components/nrg-search/controller.js
+++ tests/dummy/app/view-components/nrg-search/controller.js
@@ -1,48 +1,39 @@
 // BEGIN-SNIPPET querying-search
-import { A } from '@ember/array';
 import Controller from '@ember/controller';
-import { alias } from '@ember/object/computed';
-import { task, timeout } from 'ember-concurrency';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { timeout } from 'ember-concurrency';
 
-export default Controller.extend({
-  options: A([
-    {
-      animal: 'rabbit',
-      header: 'Alfred',
-      description: 'male',
-    },
-    {
-      animal: 'rabbit',
-      header: 'Betty',
-      description: 'female',
-    },
-    {
-      animal: 'dog',
-      header: 'Bobby',
-      description: 'male',
-    },
-    {
-      animal: 'dog',
-      header: 'Harley',
-      description: 'female',
-    },
-    {
-      animal: 'cat',
-      header: 'Sam',
-      description: 'male',
-    },
-    {
-      animal: 'cat',
-      header: 'Sally',
-      description: 'female',
-    },
-  ]),
+const options = [
+  {
+    animal: 'rabbit',
+    header: 'Alfred',
+    description: 'male',
+  },
+  {
+    animal: 'rabbit',
+    header: 'Betty',
+    description: 'female',
+  },
+  {
+    animal: 'dog',
+    header: 'Bobby',
+    description: 'male',
+  },
+];
 
-  loading: alias('queryTask.isRunning'),
+export default class ViewComponentsNrgSearchController extends Controller {
+  @tracked
+  queryString = null;
 
-  queryTask: task(function*() {
-    yield timeout(2000);
-    this.set('items', this.options.toArray());
-  }),
-});
+  @tracked
+  selectedResult = null;
+
+  @action
+  async query(queryString) {
+    this.queryString = queryString;
+    await timeout(2000);
+    return options;
+  }
+}
 // END-SNIPPET querying-search
--- tests/integration/components/nrg-search/component-test.js
+++ tests/integration/components/nrg-search/component-test.js
@@ -1,42 +1,45 @@
-import { fillIn, find, findAll, render } from '@ember/test-helpers';
+import { fillIn, findAll, render } from '@ember/test-helpers';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
 
-module('Integration | Component | nrg-search', function(hooks) {
+module('Integration | Component | nrg-search', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders selected value', async function (assert) {
     this.selected = {
       header: 'header',
     };
-    await render(hbs`<NrgSearch @selected={{selected}} />`);
+    await render(hbs`<NrgSearch @model={{this}} @valuePath="selected" />`);
     assert.dom('input').hasValue('header');
   });
 
-  test('value is populated after it is loaded', async function(assert) {
-    this.selected = {
-      isLoading: true,
+  test('query method is called with querystring', async function (assert) {
+    assert.expect(1);
+    this.query = (querystring) => {
+      assert.equal(querystring, 'search');
     };
-    await render(hbs`<NrgSearch @selected={{selected}} />`);
-    assert.dom('input').hasValue('');
-    this.set('selected.header', 'header');
-    this.set('selected.isLoading', false);
-    assert.dom('input').hasValue('header');
-  });
-
-  test('results do not display when loading', async function(assert) {
-    this.items = [{}];
-    this.loading = true;
-    await render(hbs`<NrgSearch @items={{items}} @loading={{loading}} />`);
-    assert.notOk(findAll('.results').length);
-  });
-
-  test('query action is sent', async function(assert) {
-    this.queryAction = searchString => {
-      assert.equal(searchString, 'search');
-    };
-    await render(hbs`<NrgSearch @query={{action queryAction}} />`);
+    await render(hbs`<NrgSearch @query={{this.query}} />`);
     await fillIn('input', 'search');
+  });
+
+  test('results display after query', async function (assert) {
+    this.query = () => {
+      return [{}];
+    };
+    await render(hbs`<NrgSearch @searchTimeout={{0}} @query={{query}} />`);
+    await fillIn('input', 'querystring');
+    assert.ok(findAll('.results').length);
+  });
+
+  test('results do not display when loading', async function (assert) {
+    this.query = () => {
+      return [{}];
+    };
+    await render(
+      hbs`<NrgSearch @searchTimeout={{0}} @query={{query}} @loading={{true}} />`
+    );
+    await fillIn('input', 'querystring');
+    assert.notOk(findAll('.results').length);
   });
 });
