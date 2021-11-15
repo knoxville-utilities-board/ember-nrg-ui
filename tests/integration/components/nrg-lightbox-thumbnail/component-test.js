5db038c00684884e694ba40b3848a425eb37d949
Keith <keith.clinard@kub.org>
2021-11-15 14:37:11 -0500

    feat: convert nrg-lightbox-thumbnail to glimmer component (#338)

--- tests/integration/components/nrg-lightbox-thumbnail/component-test.js
+++ tests/integration/components/nrg-lightbox-thumbnail/component-test.js
@@ -1,12 +1,12 @@
-import { find, render } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
 
-module('Integration | Component | nrg-lightbox-thumbnail', function(hooks) {
+module('Integration | Component | nrg-lightbox-thumbnail', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     this.photo = {
       url: 'http://someUrl',
     };
