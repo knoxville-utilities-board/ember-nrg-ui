d38fb24a8c7d6a41f21e169257ae5a25b4a47f3f
Keith <keith.clinard@kub.org>
2021-11-15 16:46:26 -0500

    feat: convert nrg-appbar to glimmer component (#336)
    * feat: convert nrg-appbar to glimmer component
    
    * chore: Update tests
    
    * chore: PR Review

--- tests/integration/components/nrg-appbar/component-test.js
+++ tests/integration/components/nrg-appbar/component-test.js
@@ -1,37 +1,50 @@
-import { find, render } from '@ember/test-helpers';
+import Service from '@ember/service';
+import { render } from '@ember/test-helpers';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
-import ENV from '../../../../config/environment';
 
-module('Integration | Component | nrg-appbar', function(hooks) {
+module('Integration | Component | nrg-appbar', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('title renders', async function(assert) {
-    await render(hbs`<NrgAppbar @title="test title" />`);
+  test('title renders', async function (assert) {
+    class ApplicationServiceStub extends Service {
+      environmentConfig = {
+        'ember-nrg-ui': {
+          productionEnvironments: ['prod'],
+        },
+      };
+      pageTitle = 'test title';
+    }
+    this.owner.register('service:application', ApplicationServiceStub);
+    await render(hbs`<NrgAppbar/>`);
 
     assert.dom('.header.item').hasText('test title');
   });
 
-  test('non-production environments display', async function(assert) {
-    ENV['ember-nrg-ui'] = {
-      productionEnvironments: [],
-    };
-    this.set('applicationSettings', {
-      localEnvironment: 'bob',
-    });
-    await render(hbs`<NrgAppbar @applicationSettings={{applicationSettings}} @title="test title" />`);
-    assert.dom('.environment-title').hasText('BOB');
+  test('non-production environments display', async function (assert) {
+    class ApplicationServiceStub extends Service {
+      environmentConfig = {
+        'ember-nrg-ui': {
+          productionEnvironments: ['bob'],
+        },
+      };
+    }
+    this.owner.register('service:application', ApplicationServiceStub);
+    await render(hbs`<NrgAppbar />`);
+    assert.dom('.environment-title').hasText('PROD');
   });
 
-  test('production evironment should not display', async function(assert) {
-    ENV['ember-nrg-ui'] = {
-      productionEnvironments: ['bob'],
-    };
-    this.set('applicationSettings', {
-      localEnvironment: 'bob',
-    });
-    await render(hbs`<NrgAppbar @applicationSettings={{applicationSettings}} @title="test title" />`);
+  test('production evironment should not display', async function (assert) {
+    class ApplicationServiceStub extends Service {
+      environmentConfig = {
+        'ember-nrg-ui': {
+          productionEnvironments: ['prod'],
+        },
+      };
+    }
+    this.owner.register('service:application', ApplicationServiceStub);
+    await render(hbs`<NrgAppbar />`);
     assert.dom('.environment-title').doesNotExist();
   });
 });
