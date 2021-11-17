3a939788c014f6ebc0e959516fdb4f4fdd36121f
Keith <keith.clinard@kub.org>
2021-11-17 08:45:09 -0500

    feat: Replace route-page-title mixin (#337)
    * feat: Replace route-page-title mixin
    
    * feat: Add page titles to dummy app
    
    * chore: Fix rebase error

--- config/environment.js
+++ config/environment.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  return {};
+  return {
+    pageTitle: {
+      prepend: false,
+      replace: true,
+    },
+  };
 };
