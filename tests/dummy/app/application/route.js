e391720f13d733dade4919df6ee07787f349d352
jkc07100 <keith.clinard@kub.org>
2021-11-17 08:38:22 -0500

    chore: Fix rebase error

--- tests/dummy/app/application/route.js
+++ tests/dummy/app/application/route.js
@@ -4,19 +4,6 @@ import KeyboardShortcutMixin from 'ember-nrg-ui/mixins/global-keyboard-shortcut'
 export default Route.extend(KeyboardShortcutMixin, {
   pageTitle: 'NRG UI',
 
-  contextItems: [
-    {
-      label: 'Release Notes',
-      actionName: 'routeToReleaseNotes',
-      priority: 2,
-    },
-    {
-      label: 'More Release Note',
-      actionName: 'routeToReleaseNotes',
-      priority: 2,
-    },
-  ],
-
   keyboardShortcuts: [
     {
       key: 'KeyS',
