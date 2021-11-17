3c1223eeab6b542235fec690040d6476dce35624
Keith <keith.clinard@kub.org>
2021-11-17 15:55:28 -0500

    feat: Replace nrg-master-detail with new glimmer component (#348)

--- addon/components/nrg-master-detail/master.js
+++
@@ -1,7 +0,0 @@
-import Component from '@ember/component';
-import layout from '../../templates/components/nrg-master-detail/master';
-
-export default Component.extend({
-  layout,
-  classNames: ['ui', 'segment', 'master-detail--master'],
-});
