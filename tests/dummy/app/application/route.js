0309578f7aa17ca2f50704f9fb4e1c4843f9b722
Keith <keith.clinard@kub.org>
2021-11-17 10:48:54 -0500

    feat: Remove global-keyboard-shortcut mixin (#347)

--- tests/dummy/app/application/route.js
+++ tests/dummy/app/application/route.js
@@ -1,24 +1,11 @@
 import Route from '@ember/routing/route';
-import KeyboardShortcutMixin from 'ember-nrg-ui/mixins/global-keyboard-shortcut';
 
-export default Route.extend(KeyboardShortcutMixin, {
+export default Route.extend({
   pageTitle: 'NRG UI',
-
-  keyboardShortcuts: [
-    {
-      key: 'KeyS',
-      alt: true,
-      actionName: 'goToKeyboardShortcuts',
-      description: 'Go to keyboard-shortcuts',
-    },
-  ],
 
   actions: {
     routeToReleaseNotes() {
       this.transitionTo('release-notes');
-    },
-    goToKeyboardShortcuts() {
-      this.transitionTo('view-mixins.global-keyboard-shortcut');
     },
   },
 });
