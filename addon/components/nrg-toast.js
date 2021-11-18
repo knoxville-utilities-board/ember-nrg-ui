6991ca16b5a9ee2e8db490eb7c6b10b1d5251cee
Tyler Senter <tyler.senter.427@gmail.com>
2021-11-18 17:20:03 -0500

    chore(toast): Remove classic computed property

--- addon/components/nrg-toast.js
+++ addon/components/nrg-toast.js
@@ -1,21 +1,18 @@
-import { alias } from '@ember/object/computed';
 import { htmlSafe } from '@ember/string';
 import FlashMessage from 'ember-cli-flash/components/flash-message';
 
 export default class Toast extends FlashMessage {
-  @alias('toast') flash;
-
   get showProgress() {
-    const timeout = this.get('toast.timeout');
-    const showProgress = this.get('toast.showProgress');
-    const sticky = this.get('toast.sticky');
+    const timeout = this.flash.timeout;
+    const showProgress = this.flash.showProgress;
+    const sticky = this.flash.sticky;
     return showProgress && timeout && !sticky;
   }
 
   get progressDuration() {
-    const timeout = this.get('toast.timeout');
+    const timeout = this.flash.timeout;
     if (!this.showProgress) {
-      return;
+      return '';
     }
     return htmlSafe(`animation-duration: ${timeout}ms;`);
   }
