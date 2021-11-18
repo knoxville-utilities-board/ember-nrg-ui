c32bd3cff2b0d10ba9ebb8c0a76c2ac5660310fd
KeithClinard <keith@clinard.dev>
2021-11-18 09:40:01 -0500

    chore: Add role security

--- addon/components/nrg-sidebar-menu/item.js
+++ addon/components/nrg-sidebar-menu/item.js
@@ -1,9 +1,27 @@
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
 export default class NrgSidebarMenuItem extends Component {
+  @service application;
+
   get url() {
     return this.args.url ?? '';
+  }
+
+  get visible() {
+    if (!this.args.role) {
+      return true;
+    }
+    const roles = Array.isArray(this.args.role)
+      ? this.args.role
+      : [this.args.role];
+    const needsAllRoles = this.args.needsAllRoles;
+    const currentUserContent = this.application.currentUser.content;
+    if (needsAllRoles) {
+      return roles.every((role) => currentUserContent.hasRole(role));
+    }
+    return roles.some((role) => currentUserContent.hasRole(role));
   }
 
   @action
