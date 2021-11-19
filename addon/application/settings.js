28d81a2ff8f2538b80bb90233411076417a2bfe1
jkc07100 <keith.clinard@kub.org>
2021-11-19 09:38:08 -0500

    feat: Nest user and settings under application service

--- addon/application/settings.js
+++
@@ -1,4 +0,0 @@
-import EmberObject from '@ember/object';
-import Evented from '@ember/object/evented';
-
-export default class ApplicationSettings extends EmberObject.extend(Evented) {}
--- tests/integration/components/nrg-home-cards/home-card/component-test.js
+++ tests/integration/components/nrg-home-cards/home-card/component-test.js
@@ -1,32 +1,29 @@
-import ObjectProxy from '@ember/object/proxy';
-import { find, render } from '@ember/test-helpers';
+import Service from '@ember/service';
+import { render } from '@ember/test-helpers';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
 
-module('Integration | Component | nrg-home-cards/home-card', function(hooks) {
-  setupRenderingTest(hooks);
-  hooks.beforeEach(function() {
-    const currentUser = ObjectProxy.create({
-      content: {
-        hasRole(role) {
-          return role === 'users';
-        },
-      },
-    });
-    this.owner.register('user:current', currentUser, {
-      instantiate: false,
-      singleton: true,
-    });
-    this.owner.inject('service', 'currentUser', 'user:current');
-  });
+class ApplicationServiceStub extends Service {
+  environmentConfig = {};
+  user = {
+    hasRole(role) {
+      return role === 'users';
+    },
+  };
+}
 
-  test('hidden if user does not have role', async function(assert) {
+module('Integration | Component | nrg-home-cards/home-card', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('hidden if user does not have role', async function (assert) {
+    this.owner.register('service:application', ApplicationServiceStub);
     await render(hbs`<NrgHomeCards::HomeCard @role="administrators" />`);
     assert.dom('.home-card').hasClass('is-visually-hidden');
   });
 
-  test('show if user has role', async function(assert) {
+  test('show if user has role', async function (assert) {
+    this.owner.register('service:application', ApplicationServiceStub);
     await render(hbs`<NrgHomeCards::HomeCard @role="users" />`);
     assert.dom('.home-card').hasNoClass('is-visually-hidden');
   });
