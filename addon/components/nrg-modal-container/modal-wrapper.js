cda802ad7a4682ffc527fe83f6e1bc47d6d8343f
Tyler Small <tsmall@smallinitiatives.com>
2021-11-19 15:14:42 -0500

    feat: convert modal-wrapper to glimmer component (#362)
    * feat: convert modal-wrapper to glimmer component
    
    * fix: accessibility
    
    * fix: PR review
    
    Co-authored-by: Keith <keith.clinard@kub.org>
    
    Co-authored-by: Keith <keith.clinard@kub.org>

--- addon/components/nrg-modal-container/modal-wrapper.js
+++ addon/components/nrg-modal-container/modal-wrapper.js
@@ -1,158 +1,135 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { alias, and, not, readOnly } from '@ember/object/computed';
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { htmlSafe } from '@ember/string';
-import layout from '../../templates/components/nrg-modal-container/modal-wrapper';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 const isIE11 = !window.ActiveXObject && 'ActiveXObject' in window;
 const useFlexBox = !isIE11;
 
-export default Component.extend({
-  layout,
+export default class ModalWrapper extends Component {
+  @service('modal') modalService;
+  @tracked _parentContentNode;
+  @tracked modalElement;
+  @tracked contentNode;
 
-  tagName: '',
+  get isTesting() {
+    return this.args.modal?.isTesting;
+  }
 
-  modalService: service('modal'),
+  get isOpen() {
+    return this.args.modal?.isOpen;
+  }
 
-  isTesting: readOnly('modal.isTesting'),
-  isOpen: alias('modal.isOpen'),
-  isActive: alias('modal.isActive'),
-  renderInPlace: alias('modal.renderInPlace'),
-  hasMovedDom: alias('modal.hasMovedDom'),
+  get isActive() {
+    return this.args.modal?.isActive;
+  }
 
-  takeover: alias('modal.takeover'),
-  sidebar: alias('modal.sidebar'),
-  basic: alias('modal.basic'),
-  lightbox: alias('modal.lightbox'),
-  modalClass: alias('modal.modalClass'),
-  dimmerClass: alias('modal.dimmerClass'),
-  scrolling: alias('modal.scrolling'),
-  notSidebar: not('sidebar'),
-  hasCloseIcon: and('dismissable', 'notSidebar'),
-  dismissable: alias('modal.dismissable'),
+  get renderInPlace() {
+    return this.args.modal?.renderInPlace;
+  }
 
-  modalStyles: computed('modalElement', function() {
-    if (useFlexBox || !this.modalElement || this.takeover || this.sidebar || this.lightbox) {
+  get hasMovedDom() {
+    return this.args.modal?.hasMovedDom;
+  }
+
+  get takeover() {
+    return this.args.modal?.takeover;
+  }
+
+  get sidebar() {
+    return this.args.modal?.sidebar;
+  }
+
+  get basic() {
+    return this.args.modal?.basic;
+  }
+
+  get lightbox() {
+    return this.args.modal?.lightbox;
+  }
+
+  get scrolling() {
+    return this.args.modal?.scrolling;
+  }
+
+  get hasCloseIcon() {
+    return this.args.modal?.dismissable && !this.args.modal?.sidebar;
+  }
+
+  get modalContent() {
+    return this.modalElement?.querySelector('.modal-content');
+  }
+
+  get marginTop() {
+    return this.modalElement?.offsetHeight / 2;
+  }
+
+  get marginLeft() {
+    return this.modalElement?.offsetWidth / 2;
+  }
+
+  get modalStyles() {
+    if (
+      useFlexBox ||
+      !this.modalElement ||
+      this.takeover ||
+      this.sidebar ||
+      this.lightbox
+    ) {
       return '';
     }
-    const marginTop = this.modalElement.offsetHeight / 2;
-    const marginLeft = this.modalElement.offsetWidth / 2;
     return htmlSafe(
-      `margin-top: -${marginTop}px; margin-left: -${marginLeft}px;`
+      `margin-top: -${this.marginTop}px; margin-left: -${this.marginLeft}px;`
     );
-  }),
+  }
 
-  onResize() {
-    this.notifyPropertyChange('modalStyles');
-  },
-
-  _modalClass: computed(
-    'sidebar',
-    'basic',
-    'lightbox',
-    'modalClass',
-    'takeover',
-    function() {
-      const appliedClasses = [];
-      if (this.sidebar) {
-        appliedClasses.push('sidebar-modal');
-      }
-      if (this.lightbox) {
-        appliedClasses.push('fullscreen');
-        appliedClasses.push('lightbox');
-      }
-      if (this.modalClass) {
-        appliedClasses.push(this.modalClass);
-      }
-      if (this.basic) {
-        appliedClasses.push('basic');
-      }
-      if (this.takeover) {
-        appliedClasses.push('side-by-side--takeover');
-      }
-      return appliedClasses.join(' ');
-    }
-  ),
-
-  _contentClass: computed(
-    'sidebar',
-    'lightbox',
-    'renderInPlace',
-    'takeover',
-    function() {
-      const appliedClasses = ['modal-content'];
-      if (this.lightbox) {
-        appliedClasses.push('image');
-      }
-      if (!this.sidebar) {
-        appliedClasses.push('content');
-      }
-      if (this.scrolling) {
-        appliedClasses.push('scrolling');
-      }
-      return appliedClasses.join(' ');
-    }
-  ),
-
+  @action
   addModalToWormhole(element) {
     if (this.hasMovedDom || this.renderInPlace || this.isTesting) {
       return;
     }
-    this.contentNode = this.modal.element.querySelector('.modal-js');
+    this.contentNode = this.args.modal?.element?.querySelector('.modal-js');
     this._parentContentNode = element;
     element.appendChild(this.contentNode);
-    this.set('hasMovedDom', true);
-  },
+    this.args.modal.hasMovedDom = true;
+  }
 
+  @action
   removeModalFromWormhole() {
     if (!this.hasMovedDom) {
       return;
     }
-    if (!this.modal || this.modal.isDestroying || !this.modal.element) {
+    if (
+      !this.args.modal ||
+      this.args.modal?.isDestroying ||
+      !this.args.modal?.element
+    ) {
       this._parentContentNode.removeChild(this.contentNode);
     } else {
-      this.modal.element.appendChild(this.contentNode);
-      this.modalService.remove(this);
-      this.set('hasMovedDom', false);
+      this.args.modal?.element?.appendChild(this.contentNode);
+      this.modalService?.remove(this);
+      this.args.modal.hasMovedDom = false;
     }
-  },
+  }
 
+  @action
   modalElementAdded(element) {
-    this.set('modalElement', element);
+    this.modalElement = element;
+  }
 
-    if (useFlexBox || this.takeover || this.sidebar || this.lightbox) {
-      return;
-    }
-
-    const config = {
-      attributes: false,
-      childList: true,
-      subtree: true,
-    };
-    this.modalMutationObserver = new MutationObserver(() => {
-      this.notifyPropertyChange('modalStyles');
-    });
-    const observedElement = element.querySelector('.modal-content');
-    this.modalMutationObserver.observe(observedElement, config);
-  },
-
-  willDestroyElement() {
-    this._super(...arguments);
-    if (this.modalMutationObserver) {
-      this.modalMutationObserver.disconnect();
-    }
-  },
-
+  @action
   onHide() {
-    this.modal.onHide();
-  },
+    this.args.modal?.onHide?.();
+  }
 
+  @action
   _onPrimary() {
-    this.modal._onPrimary();
-  },
+    this.args.modal?._onPrimary?.();
+  }
 
+  @action
   _onSecondary() {
-    this.modal._onSecondary();
-  },
-});
+    this.args.modal?._onSecondary?.();
+  }
+}
