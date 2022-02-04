650d4f1167b8a91129c09905933f054bd25cb89c
Nadeem Hemani <67244995+nadeemhemani@users.noreply.github.com>
2022-02-04 09:37:49 -0500

    fix: add package to project (#390)
    * fix: add package to project
    
    * fix: add package to project
    
    * fix: add package to project

--- blueprints/ember-nrg-ui/index.js
+++ blueprints/ember-nrg-ui/index.js
@@ -43,16 +43,18 @@ module.exports = {
     const blueprint = this;
     const nodePackages = [
       {
+        name: 'sass',
+        target: '1.43.5',
+      },
+    ];
+    const addOns = [
+      {
         name: 'ember-cli-mirage',
         target: '2.2.0',
       },
       {
         name: 'ember-cli-sass',
         target: '10.0.1',
-      },
-      {
-        name: 'sass',
-        target: '1.43.5',
       },
     ];
 
@@ -85,6 +87,11 @@ module.exports = {
       })
       .then(function () {
         return blueprint.addAddonsToProject({
+          packages: addOns,
+        });
+      })
+      .then(function () {
+        return blueprint.addPackageToProject({
           packages: nodePackages,
         });
       })
