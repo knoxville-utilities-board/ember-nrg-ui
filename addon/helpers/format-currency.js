5564975858d9765746de04fdbabe498f7463ceea
F. DeWayne Lane <fdlane@hotmail.com>
2022-02-06 09:04:26 -0500

    refactor: default currency 2 digit precision

--- addon/helpers/format-currency.js
+++ addon/helpers/format-currency.js
@@ -2,13 +2,13 @@ import { helper } from '@ember/component/helper';
 import { formatNumber } from './format-number';
 import unformat from './unformat';
 
-export function formatCurrency(value) {
-  const formattedValue = formatNumber(value, 2);
+export function formatCurrency(value, precision = 2) {
+  const formattedValue = formatNumber(value, precision);
   return unformat(value) < 0
     ? '($' + formattedValue + ')'
     : '$' + formattedValue;
 }
 
-export default helper(([value]) => {
-  return formatCurrency(value);
+export default helper(([value], { precision }) => {
+  return formatCurrency(value, precision);
 });
