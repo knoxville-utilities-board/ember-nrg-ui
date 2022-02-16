909fc9d8b0beb12959d094670a50addad88e30d3
tsmalls93 <tsmall@smallinitiatives.com>
2022-02-16 15:44:04 -0500

    fix: nrg-toast progress and dismissal

--- addon/components/nrg-toast.js
+++ addon/components/nrg-toast.js
@@ -1,9 +1,13 @@
 import { htmlSafe } from '@ember/template';
 import FlashMessage from 'ember-cli-flash/components/flash-message';
 export default class Toast extends FlashMessage {
+  get flash() {
+    return this.toast;
+  }
+
   get progressDuration() {
-    const timeout = this.flash.timeout;
-    if (!this.showProgress) {
+    const timeout = this.toast?.timeout;
+    if (!this.toast.showProgress) {
       return '';
     }
     return htmlSafe(`animation-duration: ${timeout}ms;`);
