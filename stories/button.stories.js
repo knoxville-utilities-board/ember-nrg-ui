b9a17b52b8b4d3bb04ac0f3c40fcf0892aadd7d5
Michael Kaufman <mkaufman94@gmail.com>
2022-03-04 15:57:31 -0600

    feat: Add stories for NrgButton

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -9,7 +9,8 @@ export default {
 };
 
 const Template = (args) => ({
-  template: hbs`<NrgButton @text={{this.text}} @disabled={{this.disabled}} />`,
+  template: hbs`<NrgButton @text={{this.text}} @disabled={{this.disabled}} @loading={{this.loading}} @autofocus={{this.autofocus}}  @class={{this.class}}
+    @icon={{this.icon}} @onClick={{this.onClick}} />`,
   context: args,
 });
 
@@ -17,4 +18,58 @@ export const Default = Template.bind({});
 Default.args = {
   text: 'Hover Me',
   disabled: false,
+  loading: false,
+  autofocus: false,
+  class: '',
+  onClick: () => {
+    alert('You clicked the button!');
+  },
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  text: 'Primary',
+  disabled: false,
+  loading: false,
+  autofocus: false,
+  class: 'primary',
+  onClick: () => {
+    alert('You clicked the button!');
+  },
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  text: 'Secondary',
+  disabled: false,
+  loading: false,
+  autofocus: false,
+  class: 'secondary',
+  onClick: () => {
+    alert('You clicked the button!');
+  },
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  text: 'Disabled',
+  disabled: true,
+  loading: false,
+  autofocus: false,
+  class: '',
+  onClick: () => {
+    alert('You clicked the button!');
+  },
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  text: 'Loading',
+  disabled: false,
+  loading: true,
+  autofocus: false,
+  class: '',
+  onClick: () => {
+    alert('You clicked the button!');
+  },
 };
