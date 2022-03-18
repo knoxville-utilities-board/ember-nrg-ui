80db597ed676962c6161bd33b2227173147d5612
Michael Kaufman <mkaufman94@gmail.com>
2022-03-18 09:26:40 -0500

    refactor: Remove unneeded wildcards for storybook build

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,4 @@
 module.exports = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../stories/**/*.stories.@(js)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
