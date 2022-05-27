c53be49386543220d7684257fcc1c1cc66f2a5dd
Tyler Senter <tyler.senter.427@gmail.com>
2022-05-27 17:10:04 -0400

    feat(validations): Support nested value paths (#417)
    * feat(validations): Support nested value paths
    
    * chore: Create tests
    
    * refactor: Use test selector to improve tests
    
    Co-authored-by: tsr07843 <tyler.senter@kub.org>

--- tests/acceptance/form-validation-test.js
+++ tests/acceptance/form-validation-test.js
@@ -10,7 +10,7 @@ module('Acceptance | form validation', function (hooks) {
     await click('button[type=submit]');
     const errorNodes = findAll('.error');
 
-    assert.equal(errorNodes.length, 7);
+    assert.equal(errorNodes.length, 8);
   });
 
   test('Custom validators work', async function (assert) {
@@ -18,18 +18,34 @@ module('Acceptance | form validation', function (hooks) {
     await click('button[type=submit]');
 
     assert
-      .dom('div.field:last-of-type > div.red.label')
+      .dom('[data-test-custom-validator] > div.red.label')
       .hasText('This is an invalid value', 'Custom error message works');
 
-    await fillIn('div.field.error:last-of-type input', 'defaultError');
+    await fillIn('[data-test-custom-validator] input', 'defaultError');
 
     assert
-      .dom('div.field:last-of-type > div.red.label')
+      .dom('[data-test-custom-validator] > div.red.label')
       .hasText('This field is not valid', 'false displays default error');
 
-    await fillIn('div.field.error:last-of-type input', 'correct');
+    await fillIn('[data-test-custom-validator] input', 'correct');
     assert
-      .dom('div.field:last-of-type > div.red.label')
+      .dom('[data-test-custom-validator] > div.red.label')
       .doesNotExist('true displays no error');
+  });
+
+  test('Nested validators work', async function (assert) {
+    await visit('/validation-tests');
+    await click('button[type=submit]');
+
+    const controller = this.owner.lookup('controller:validation-tests');
+
+    assert
+      .dom('[data-test-nested-validator] > div.red.label')
+      .hasText("This field can't be blank");
+
+    await fillIn('[data-test-nested-validator] input', 'text');
+    assert.dom('[data-test-nested-validator] > div.red.label').doesNotExist();
+
+    assert.equal(controller.nested.field, 'text', 'nested value paths work');
   });
 });
