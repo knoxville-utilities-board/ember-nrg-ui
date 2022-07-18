4ecc6f0242cd81316ce012021dd585ad224b4797
mpl08140 <mananpatel6902@gmail.com>
2022-07-18 09:26:23 -0400

    style: Run formatter

--- blueprints/ember-nrg-ui/files/__config__/content-security-policy.js
+++ blueprints/ember-nrg-ui/files/__config__/content-security-policy.js
@@ -1,19 +1,19 @@
 module.exports = function () {
-    return {
-      delivery: ['header'],
-      enabled: true,
-      failTests: true,
-      policy: {
-        'default-src': ["'none'"],
-        'connect-src': ["'self'"],
-        'script-src': ["'self' 'unsafe-eval'"],
-        'script-src-elem': ["'self' 'unsafe-inline'",],
-        'img-src': ["'self'"],
-        'font-src': ["'self' data:"],
-        'style-src': ["'unsafe-inline'"],
-        'style-src-elem': ["'self'"],
-        'report-uri': ['/'],
-      },
-      reportOnly: true,
-    };
+  return {
+    delivery: ['header'],
+    enabled: true,
+    failTests: true,
+    policy: {
+      'default-src': ["'none'"],
+      'connect-src': ["'self'"],
+      'script-src': ["'self' 'unsafe-eval'"],
+      'script-src-elem': ["'self' 'unsafe-inline'"],
+      'img-src': ["'self'"],
+      'font-src': ["'self' data:"],
+      'style-src': ["'unsafe-inline'"],
+      'style-src-elem': ["'self'"],
+      'report-uri': ['/'],
+    },
+    reportOnly: true,
   };
+}
