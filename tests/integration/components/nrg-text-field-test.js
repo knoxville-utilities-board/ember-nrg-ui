eff85f51b649a282fe5ecd475f8638429669fa26
Michael Kaufman <mkaufman94@gmail.com>
2022-08-30 09:20:42 -0500

    test: Write test for disabling two way binding

--- tests/integration/components/nrg-text-field-test.js
+++ tests/integration/components/nrg-text-field-test.js
@@ -1,4 +1,4 @@
-import { render } from '@ember/test-helpers';
+import { fillIn, render } from '@ember/test-helpers';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
@@ -10,6 +10,28 @@ module('Integration | Component | nrg-text-field', function (hooks) {
     this.set('name', 'bob');
 
     await render(hbs`<NrgTextField @model={{this}} @valuePath="name" />`);
+
+    assert.dom('input').hasValue('bob');
+  });
+
+  test("can update text field's value", async function (assert) {
+    this.set('name', 'bob');
+
+    await render(hbs`<NrgTextField @model={{this}} @valuePath="name" />`);
+
+    await fillIn('input', 'lorem');
+
+    assert.dom('input').hasValue('lorem');
+  });
+
+  test('disabling two way binding prevents updates', async function (assert) {
+    this.set('name', 'bob');
+
+    await render(
+      hbs`<NrgTextField @model={{this}} @valuePath="name" @twoWayBinding={{false}} />`
+    );
+
+    await fillIn('input', 'lorem');
 
     assert.dom('input').hasValue('bob');
   });
