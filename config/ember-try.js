f37a2284f21eab34deb4a634e48b9bd8005fed51
tsr07843 <tyler.senter@kub.org>
2022-09-09 17:38:26 -0400

    chore(ci): Force ember-try to use Yarn

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,6 +6,7 @@ const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
+    useYarn: true,
     scenarios: [
       {
         name: 'ember-lts-3.28',
