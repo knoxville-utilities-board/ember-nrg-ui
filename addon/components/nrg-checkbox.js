3d6020364f8f736d53a781c729ebbe838e4253df
tsr07843 <tyler.senter@kub.org>
2022-09-11 12:50:20 -0400

    refactor: Remove dependency on internal guidFor with unique-id helper

--- addon/components/nrg-checkbox.js
+++ addon/components/nrg-checkbox.js
@@ -1,12 +1,7 @@
 import { action } from '@ember/object';
-import { guidFor } from '@ember/object/internals';
-import { tracked } from '@glimmer/tracking';
 import NrgValidationComponent from './nrg-validation-component';
 
 export default class NrgCheckboxComponent extends NrgValidationComponent {
-  @tracked
-  focusId = 'field-' + guidFor(this);
-
   get checked() {
     return !!this.value;
   }
--- addon/components/nrg-form-field.js
+++ addon/components/nrg-form-field.js
@@ -1,11 +1,7 @@
-import { guidFor } from '@ember/object/internals';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class NrgFormFieldComponent extends Component {
-  @tracked
-  focusId = 'field-' + guidFor(this);
-
   @tracked
   model;
 
