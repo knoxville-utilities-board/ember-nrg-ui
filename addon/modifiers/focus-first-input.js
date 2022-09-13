f4e9eb2262d50457bae0406aa42a7f381f08dc72
Tyler Senter <tyler.senter.427@gmail.com>
2022-09-13 13:53:06 -0400

    chore: Fix modifier deprecations (#435)
    * chore: Bump ember-keyboard
    
    * fix: Remove no-args-property and no-element-property deprecations
    
    Co-authored-by: tsr07843 <tyler.senter@kub.org>

--- addon/modifiers/focus-first-input.js
+++ addon/modifiers/focus-first-input.js
@@ -1,8 +1,8 @@
 import Modifier from 'ember-modifier';
 
 export default class FocusFirstInputModifier extends Modifier {
-  didInstall() {
-    const inputs = this.element.querySelectorAll('input[type=text]:enabled');
+  modify(element) {
+    const inputs = element.querySelectorAll('input[type=text]:enabled');
     inputs?.[0]?.focus();
   }
 }
