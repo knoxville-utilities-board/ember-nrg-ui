aa8d855dd69d3fde2542de7c1135dea6bb07dd18
Jayden Leuciuc <68298467+jayden-leuciuc@users.noreply.github.com>
2022-12-07 10:51:07 -0500

    chore: Change image provider (#462)
    * chore: Change image provider
    
    * chore: Bump package version

--- tests/dummy/app/controllers/view-components/nrg-lightbox.js
+++ tests/dummy/app/controllers/view-components/nrg-lightbox.js
@@ -13,15 +13,15 @@ export default class ViewComponentsNrgLightboxController extends Controller {
   @tracked
   photos = [
     {
-      url: 'https://www.fillmurray.com/400/400',
+      url: 'https://www.unsplash.it/400/400',
       detail: 'Some details about the first photo\n',
     },
     {
-      url: 'https://www.fillmurray.com/100/100',
+      url: 'https://www.unsplash.it/100/100',
       detail: 'Some details about the second photo\n',
     },
     {
-      url: 'https://www.fillmurray.com/2448/3264',
+      url: 'https://www.unsplash.it/2448/3264',
       detail: 'Some details about the third photo\n',
     },
   ];
