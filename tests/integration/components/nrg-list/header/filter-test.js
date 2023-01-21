7c036ae95a6804a4937ff499f0a1dc1d9e5b4cb5
Tyler Senter <tyler.senter.427@gmail.com>
2023-01-20 22:46:00 -0500

    fix(nrg-list/header): Pass model/valuePath to filter and search for value tracking (#476)
    * fix(nrg-list/header/filter): Pass model/valuePath for value tracking
    
    * fix(nrg-list/header/search): Pass model/valuePath for value tracking
    
    * chore: Bump package version

--- tests/integration/components/nrg-list/header/filter-test.js
+++ tests/integration/components/nrg-list/header/filter-test.js
@@ -1,4 +1,5 @@
 import { click, find, render } from '@ember/test-helpers';
+import { tracked } from '@glimmer/tracking';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
@@ -24,5 +25,29 @@ module('Integration | Component | nrg-list/header/filter', function (hooks) {
     const dropdown = find('.ui.dropdown');
     await click(dropdown);
     await click(dropdown.querySelector('.item'));
+  });
+
+  test('value updates when action fires', async function (assert) {
+    class Model {
+      @tracked
+      value;
+    }
+    this.model = new Model();
+
+    const filterValue = {
+      label: 'label',
+    };
+    this.filterList = [filterValue];
+
+    await render(
+      hbs`<NrgList::Header::Filter @filters={{this.filterList}} @model={{this}} @valuePath="model.value" />`
+    );
+
+    const dropdown = find('.ui.dropdown');
+    await click(dropdown);
+    await click(dropdown.querySelector('.item'));
+
+    assert.deepEqual(this.model.value, filterValue);
+    assert.dom('.text', dropdown).containsText('label');
   });
 });
--- tests/integration/components/nrg-list/header/search-test.js
+++ tests/integration/components/nrg-list/header/search-test.js
@@ -1,4 +1,5 @@
 import { fillIn, render } from '@ember/test-helpers';
+import { tracked } from '@glimmer/tracking';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
@@ -15,5 +16,18 @@ module('Integration | Component | nrg-list/header/search', function (hooks) {
       hbs`<NrgList::Header::Search @onChange={{this.changedAction}} />`
     );
     await fillIn('input', 'value');
+  });
+
+  test('value updates when action fires', async function (assert) {
+    class Model {
+      @tracked
+      value;
+    }
+    this.model = new Model();
+    await render(
+      hbs`<NrgList::Header::Search @model={{this}} @valuePath="model.value" />`
+    );
+    await fillIn('input', 'value');
+    assert.strictEqual(this.model.value, 'value');
   });
 });
