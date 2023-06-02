550d208038185605b097d052af2a5edac1c00c78
Michael Kaufman <Michael.Kaufman@kub.org>
2023-06-02 12:42:11 -0400

    refactor: Remove unneeded controller files

--- tests/dummy/app/controllers/view-components/nrg-modal.js
+++
@@ -1,9 +0,0 @@
-import { action, set } from '@ember/object';
-import Controller from '@ember/controller';
-
-export default class NrgModalController extends Controller {
-  @action
-  setProperty(key, value) {
-    set(this, key, value);
-  }
-}
