45ae17bf0e8da88f8a49e4c581e3e00f032c953d
Michael Kaufman <Michael.Kaufman@kub.org>
2023-06-02 13:07:05 -0400

    feat: Add updated controls to datetime

--- tests/dummy/app/components/freestyle/forms/datetime.js
+++ tests/dummy/app/components/freestyle/forms/datetime.js
@@ -1,7 +1,10 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import dayjs from 'dayjs';
 
 export default class FreestyleFormsDatetimeComponent extends Component {
+  maxDate = dayjs().add(5, 'days').toDate();
+
   dateTimeTypeOptions = ['date', 'datetime'];
 
   dateTimeValue = new Date();
