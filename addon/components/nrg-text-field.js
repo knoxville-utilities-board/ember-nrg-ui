e2ec2db8bae391d3dfd2b7ceb6616a3796d05619
TSenter <tyler.senter.427@gmail.com>
2023-06-02 15:46:54 -0400

    feat(text-field): Only allow numeric input when type is number

--- addon/components/nrg-text-field.js
+++ addon/components/nrg-text-field.js
@@ -1,8 +1,31 @@
 import NrgValidationComponent from './nrg-validation-component';
+import { action } from '@ember/object';
 
 const defaultType = 'text';
 export default class NrgTextFieldComponent extends NrgValidationComponent {
   get type() {
     return this.args.type ?? defaultType;
+  }
+
+  validateInput(input) {
+    if (this.args.validateInput) {
+      return this.args.validateInput(input);
+    }
+
+    if (this.type === 'number') {
+      if (this.args.allowDecimals) {
+        return !isNaN(parseFloat(input));
+      }
+      return !isNaN(parseInt(input)) && input.indexOf('.') === -1;
+    }
+    return true;
+  }
+
+  @action
+  onChange(value) {
+    if (!this.validateInput(value)) {
+      return;
+    }
+    super.onChange(value);
   }
 }
