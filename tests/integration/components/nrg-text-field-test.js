d45fd96b8655c7bd070694dd3c205ad774ffc19e
Tyler Senter <tyler.senter.427@gmail.com>
2023-06-05 09:02:22 -0400

    feat(text-field): Only allow numeric input when type is number (#495)
    * feat(text-field): Only allow numeric input when type is number
    
    * chore: Create tests

--- tests/integration/components/nrg-text-field-test.js
+++ tests/integration/components/nrg-text-field-test.js
@@ -1,4 +1,4 @@
-import { render } from '@ember/test-helpers';
+import { fillIn, render } from '@ember/test-helpers';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
@@ -12,5 +12,84 @@ module('Integration | Component | nrg-text-field', function (hooks) {
     await render(hbs`<NrgTextField @model={{this}} @valuePath="value" />`);
 
     assert.dom('input').hasValue('bob');
+  });
+
+  test("number fields don't allow non-numeric characters", async function (assert) {
+    await render(
+      hbs`<NrgTextField @model={{this}} @valuePath="value" @type="number" />`
+    );
+
+    await fillIn('input', 'bob');
+
+    assert.dom('input').hasValue('');
+    assert.strictEqual(this.value, undefined);
+  });
+
+  test('number fields allow numeric characters', async function (assert) {
+    await render(
+      hbs`<NrgTextField @model={{this}} @valuePath="value" @type="number" />`
+    );
+
+    await fillIn('input', '123');
+
+    assert.dom('input').hasValue('123');
+    assert.strictEqual(this.value, '123');
+  });
+
+  test('number fields allow decimal characters', async function (assert) {
+    await render(
+      hbs`<NrgTextField @model={{this}} @valuePath="value" @type="number" @allowDecimals={{true}} />`
+    );
+
+    await fillIn('input', '123.45');
+
+    assert.dom('input').hasValue('123.45');
+    assert.strictEqual(this.value, '123.45');
+  });
+
+  test('number fields do not allow decimal characters', async function (assert) {
+    await render(
+      hbs`<NrgTextField @model={{this}} @valuePath="value" @type="number" />`
+    );
+
+    await fillIn('input', '123');
+    await fillIn('input', '123.4');
+
+    assert.dom('input').hasValue('123');
+    assert.strictEqual(this.value, '123');
+  });
+
+  test('number fields allow negative numbers', async function (assert) {
+    await render(
+      hbs`<NrgTextField @model={{this}} @valuePath="value" @type="number" />`
+    );
+
+    await fillIn('input', '-123');
+
+    assert.dom('input').hasValue('-123');
+    assert.strictEqual(this.value, '-123');
+  });
+
+  test('number fields allow negative decimal numbers', async function (assert) {
+    await render(
+      hbs`<NrgTextField @model={{this}} @valuePath="value" @type="number" @allowDecimals={{true}} />`
+    );
+
+    await fillIn('input', '-123.45');
+
+    assert.dom('input').hasValue('-123.45');
+    assert.strictEqual(this.value, '-123.45');
+  });
+
+  test('number fields do not allow negative decimal numbers', async function (assert) {
+    await render(
+      hbs`<NrgTextField @model={{this}} @valuePath="value" @type="number" />`
+    );
+
+    await fillIn('input', '-123');
+    await fillIn('input', '-123.4');
+
+    assert.dom('input').hasValue('-123');
+    assert.strictEqual(this.value, '-123');
   });
 });
