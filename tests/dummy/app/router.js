6be14b3d8880057eed68c974cc7b3b746e10e962
Michael Kaufman <Michael.Kaufman@kub.org>
2023-06-09 10:57:23 -0400

    feat: Add button and text-field under Forms in sidebar

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -29,6 +29,8 @@ Router.map(function () {
     this.route('nrg-render-template-block');
     this.route('nrg-search');
     this.route('nrg-context-item');
+    this.route('nrg-button');
+    this.route('nrg-text-field');
   });
   this.route('view-mixins', function () {
     this.route('focus-first-input');
