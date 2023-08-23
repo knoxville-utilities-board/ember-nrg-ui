38c740a0a72d70f3e621f2b0674579b88b655334
Tyler Senter <tyler.senter.427@gmail.com>
2023-08-23 12:19:36 -0400

    feat(app-bar): Provide multiple ways to show app version (#527)
    * chore: Remove built-in release notes route
    
    * feat(app-bar): Provide multiple ways to show app version
    
    * chore: Deprecate `showReleaseNotes` parameter for NrgAppbar
    
    * chore: Fix linting

--- addon/components/nrg-appbar.js
+++ addon/components/nrg-appbar.js
@@ -1,6 +1,7 @@
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
+import { deprecate } from '@ember/debug';
 
 export default class NrgAppbarComponent extends Component {
   @service
@@ -9,8 +10,16 @@ export default class NrgAppbarComponent extends Component {
   @service
   application;
 
-  get showReleaseNotes() {
-    return this.args.showReleaseNotes !== false;
+  get showAppVersion() {
+    deprecate(
+      'showReleaseNotes is deprecated, please use showAppVersion instead',
+      this.args.showReleaseNotes === undefined,
+      {
+        id: 'nrg-appbar.show-release-notes',
+        until: '5.0.0',
+      }
+    );
+    return this.args.showAppVersion !== false;
   }
 
   get environmentDisplay() {
