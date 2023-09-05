791d1d112642eea6afc567830bc77a3196d127f4
jayden-leuciuc <leuciucjayden@gmail.com>
2023-09-05 07:38:03 -0400

    fix: Update test

--- tests/integration/components/nrg-progress-bar-test.js
+++ tests/integration/components/nrg-progress-bar-test.js
@@ -7,8 +7,8 @@ module('Integration | Component | progress-bar', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    await render(hbs`<ProgressBar />`);
+    await render(hbs`<NrgProgressBar @progressValue={{1}} @progressMax={{5}}/>`);
 
-    assert.dom(this.element).hasText('');
+    assert.dom(this.element).hasText('Step 1 of 5');
   });
 });
