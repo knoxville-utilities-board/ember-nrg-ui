5be269027b721d605cbb15c6cb9b0ba7a927a549
jayden-leuciuc <leuciucjayden@gmail.com>
2023-09-05 07:38:03 -0400

    chore: Generate test

---
+++ tests/integration/components/progress-bar-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'dummy/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | progress-bar', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    await render(hbs`<ProgressBar />`);
+
+    assert.dom(this.element).hasText('');
+  });
+});
