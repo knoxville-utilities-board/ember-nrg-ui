29be7b4b24260e93dbbd543b2ebdfca76178ef52
jayden-leuciuc <leuciucjayden@gmail.com>
2023-09-05 07:38:03 -0400

    chore: Run formatter

--- tests/integration/components/nrg-progress-bar-test.js
+++ tests/integration/components/nrg-progress-bar-test.js
@@ -7,7 +7,9 @@ module('Integration | Component | progress-bar', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    await render(hbs`<NrgProgressBar @progressValue={{1}} @progressMax={{5}}/>`);
+    await render(
+      hbs`<NrgProgressBar @progressValue={{1}} @progressMax={{5}}/>`
+    );
 
     assert.dom(this.element).hasText('Step 1 of 5');
   });
