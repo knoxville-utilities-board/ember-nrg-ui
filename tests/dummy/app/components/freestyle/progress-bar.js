9c6d55eb9fe1f32d8281213f366414a5df50ec25
jayden-leuciuc <leuciucjayden@gmail.com>
2023-09-05 07:38:03 -0400

    chore: Setup freestyle

---
+++ tests/dummy/app/components/freestyle/progress-bar.js
@@ -0,0 +1,10 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class FreestyleNrgPopupComponent extends Component {
+  @tracked
+  maxStep = 5;
+
+  @tracked
+  currentStep = 1;
+}
