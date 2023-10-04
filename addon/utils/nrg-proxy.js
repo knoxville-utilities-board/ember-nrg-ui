56e4680ee1b5709eba907d5de55878792b47a5ae
TSenter <tyler.senter.427@gmail.com>
2023-10-04 10:54:31 -0400

    chore: Round 3 - getting somewhere...

--- addon/utils/nrg-proxy.js
+++ addon/utils/nrg-proxy.js
@@ -1,94 +1,108 @@
 import { get, set } from '@ember/object';
 import { deprecate } from '@ember/debug';
 
-export default function NrgProxy(target) {
-  const handler = {
-    get: (target, prop) => {
-      if (prop === 'content') {
-        deprecate(
-          `Accessing \`nrgProxy.content\` is deprecated. Please use nrgProxy instead.`,
-          false,
-          {
-            id: 'ember-nrg-ui.proxy.get-content',
-            until: '5.0.0',
-          }
-        );
-        return target;
-      }
-      if (prop === 'get') {
-        deprecate(
-          `Accessing \`nrgProxy.get(<property>)\` is deprecated. Please use nrgProxy.<property> instead.`,
-          false,
-          {
-            id: 'ember-nrg-ui.proxy.get',
-            until: '5.0.0',
-          }
-        );
-        return (_prop) => get(target, _prop);
-      }
-      if (prop === 'set') {
-        deprecate(
-          `Accessing \`nrgProxy.set(<property>, <value>)\` is deprecated. Please use nrgProxy.<property> = <value> instead.`,
-          false,
-          {
-            id: 'ember-nrg-ui.proxy.get-set',
-            until: '5.0.0',
-          }
-        );
-        return (_prop, value) => set(target, _prop, value);
-      }
-      return get(target, prop);
-    },
-    set: (target, prop, value) => {
-      deprecate(
-        `Setting \`nrgProxy.content\` is deprecated. Please construct a new proxy instead.`,
-        prop === 'content',
-        {
-          id: 'ember-nrg-ui.proxy.set-content',
-          until: '5.0.0',
-        }
-      );
-      set(target, prop, value);
-      return true;
-    },
-    deleteProperty: (target, prop) => {
-      delete target[prop];
-      return true;
-    },
-    has: (target, prop) => {
-      return prop in target;
-    },
-    ownKeys: (target) => {
-      return Object.keys(target);
-    },
-    getOwnPropertyDescriptor: (target, prop) => {
-      return Object.getOwnPropertyDescriptor(target, prop);
-    },
-    defineProperty: (target, prop, descriptor) => {
-      Object.defineProperty(target, prop, descriptor);
-      return true;
-    },
-    getPrototypeOf: (target) => {
-      return Object.getPrototypeOf(target);
-    },
-    setPrototypeOf: (target, prototype) => {
-      Object.setPrototypeOf(target, prototype);
-      return true;
-    },
-    isExtensible: (target) => {
-      return Object.isExtensible(target);
-    },
-    preventExtensions: (target) => {
-      Object.preventExtensions(target);
-      return true;
-    },
-    apply: (target, thisArg, argumentsList) => {
-      return target.apply(thisArg, argumentsList);
-    },
-    construct: (target, argumentsList) => {
-      return new target(...argumentsList);
-    },
-  };
+const DEFAULT_HANDLER = {
+  get: (target, prop) => {
+    return get(target, prop);
+  },
+  set: (target, prop, value) => {
+    set(target, prop, value);
+    return true;
+  },
+  deleteProperty: (target, prop) => {
+    delete target[prop];
+    return true;
+  },
+  has: (target, prop) => {
+    return prop in target;
+  },
+  ownKeys: (target) => {
+    return Object.keys(target);
+  },
+  getOwnPropertyDescriptor: (target, prop) => {
+    return Object.getOwnPropertyDescriptor(target, prop);
+  },
+  defineProperty: (target, prop, descriptor) => {
+    Object.defineProperty(target, prop, descriptor);
+    return true;
+  },
+  getPrototypeOf: (target) => {
+    return Object.getPrototypeOf(target);
+  },
+  setPrototypeOf: (target, prototype) => {
+    Object.setPrototypeOf(target, prototype);
+    return true;
+  },
+  isExtensible: (target) => {
+    return Object.isExtensible(target);
+  },
+  preventExtensions: (target) => {
+    Object.preventExtensions(target);
+    return true;
+  },
+  apply: (target, thisArg, argumentsList) => {
+    return target.apply(thisArg, argumentsList);
+  },
+  construct: (target, argumentsList) => {
+    return new target(...argumentsList);
+  },
+};
 
-  return new Proxy(target, handler);
+export default class NrgProxy {
+  #proxy = null;
+  #target = null;
+
+  constructor(target) {
+    this.#target = target;
+    this.#proxy = new Proxy(target, DEFAULT_HANDLER);
+  }
+
+  get content() {
+    deprecate(
+      `Accessing \`nrgProxy.content\` is deprecated. Please use nrgProxy instead.`,
+      false,
+      {
+        id: 'ember-nrg-ui.proxy.get-content',
+        until: '5.0.0',
+      }
+    );
+    return this.#target;
+  }
+
+  set content(value) {
+    deprecate(
+      `Setting \`nrgProxy.content\` is deprecated. Please construct a new proxy instead.`,
+      false,
+      {
+        id: 'ember-nrg-ui.proxy.set-content',
+        until: '5.0.0',
+      }
+    );
+    this.#target = value;
+    this.#proxy = new Proxy(value, DEFAULT_HANDLER);
+  }
+
+  get(property) {
+    deprecate(
+      `Accessing \`nrgProxy.get('${property}')\` is deprecated. Please use \`nrgProxy.${property}\` instead.`,
+      false,
+      {
+        id: 'ember-nrg-ui.proxy.get',
+        until: '5.0.0',
+      }
+    );
+    return get(this.#proxy, property);
+  }
+
+  set(property, value) {
+    deprecate(
+      `Accessing \`nrgProxy.set('${property}', '${value}')\` is deprecated. Please use \`nrgProxy.${property} = ${value}\` instead.`,
+      false,
+      {
+        id: 'ember-nrg-ui.proxy.set',
+        until: '5.0.0',
+      }
+    );
+    set(this.#proxy, property, value);
+  }
 }
