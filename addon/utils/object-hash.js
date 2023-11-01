576f34d4ca8d2bebce5712915eaa8595cf010a83
TSenter <tyler.senter.427@gmail.com>
2023-11-01 14:20:57 -0400

    feat(object-hash): Manually serialize @ember-data models

--- addon/utils/object-hash.js
+++ addon/utils/object-hash.js
@@ -1,13 +1,31 @@
+import Model from '@ember-data/model';
+
 export function isPrimitive(val) {
   return val !== Object(val);
 }
 
 export default function (object) {
-  if (isPrimitive(object)) {
-    return String(object);
+  return JSON.stringify(object, function (key) {
+    if (isPrimitive(value)) {
+      return String(value);
+    }
+    const value = this[key];
+    if (value instanceof Object) {
+      serializeModels(value);
+      return JSON.stringify(value);
+    }
+    return value;
+  });
+}
+
+function serializeModels(obj) {
+  for (const key in obj) {
+    const val = obj[key];
+    if (val instanceof Model) {
+      obj[key] = `(${val.toString()}:${val.id})`;
+    }
+    if (String(val) === '[object Object]') {
+      serializeModels(val);
+    }
   }
-  if (object.toString) {
-    return object.toString();
-  }
-  return object;
 }
