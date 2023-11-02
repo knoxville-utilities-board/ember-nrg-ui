7aa20dcf4924674551edde4137bccfa1e718e77b
TSenter <tyler.senter.427@gmail.com>
2023-11-01 21:45:59 -0400

    chore: Override default Model#toJSON behavior (attempt 2)

--- addon/initializers/deprecation-handler.js
+++
@@ -1,6 +0,0 @@
-import { registerDeprecationHandler } from '@ember/debug';
-import { deprecationHandler } from 'ember-nrg-ui/utils/object-hash';
-
-export function initialize() {
-  registerDeprecationHandler(deprecationHandler);
-}
--- addon/utils/object-hash.js
+++ addon/utils/object-hash.js
@@ -1,13 +1,5 @@
 import Model from '@ember-data/model';
-
-let suppressDeprecations = false;
-
-export const deprecationHandler = function (message, options, next) {
-  if (suppressDeprecations) {
-    return;
-  }
-  next(message, options);
-};
+import { typeOf } from '@ember/utils';
 
 export function isPrimitive(val) {
   return val !== Object(val);
@@ -17,24 +9,59 @@ export default function (object) {
   if (isPrimitive(object)) {
     return String(object);
   }
-  suppressDeprecations = true;
-  let hash;
 
-  try {
-    hash = JSON.stringify(object, function (key, value) {
-      if (isPrimitive(value)) {
-        return value;
-      }
-      if (value instanceof Model) {
-        return `(${value.toString()}:${value.id})`;
-      }
-      return value;
-    });
-  } catch (e) {
-    console.debug('error in object-hash', e);
-  } finally {
-    suppressDeprecations = false;
-  }
+  const restore = overwriteToJSON(object);
+
+  const hash = JSON.stringify(object);
+
+  restore();
 
   return hash;
+}
+
+// This is only necessary as Ember models provide a custom toJSON
+// implementation that is currently deprecated. Once it is removed
+// from all supported versions of ember-data, this can be removed.
+function overwriteToJSON(object) {
+  const original = new Map();
+  const leaves = new WeakSet();
+
+  function restore() {
+    for (const [key, value] of original.entries()) {
+      key.toJSON = value;
+    }
+    original.clear();
+  }
+
+  try {
+    replace(object, original, leaves);
+  } catch (e) {
+    restore();
+    throw e;
+  }
+
+  return restore;
+}
+
+function replace(obj, original, leaves) {
+  if (original.has(obj)) {
+    return;
+  }
+  for (const key in obj) {
+    if (leaves.has(obj[key])) {
+      continue;
+    }
+    if (obj[key] instanceof Model) {
+      original.set(obj[key], obj[key].toJSON);
+      obj[key].toJSON = function () {
+        return `(${this.toString()}:${this.id})`;
+      };
+    }
+    if (!isPrimitive(obj[key])) {
+      leaves.add(obj[key]);
+    }
+    if (typeOf(obj[key]) === 'object') {
+      replace(obj[key], original, leaves);
+    }
+  }
 }
