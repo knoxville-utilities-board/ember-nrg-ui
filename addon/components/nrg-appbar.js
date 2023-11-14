2e8e5ca9bad045f56b7b21d981606ecceaf5112e
TSenter <tyler.senter.427@gmail.com>
2023-11-14 16:33:32 -0500

    chore: Convert to new deprecation manager

--- addon/components/nrg-appbar.js
+++ addon/components/nrg-appbar.js
@@ -1,8 +1,18 @@
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
-import { deprecate } from '@ember/debug';
+import { AddNrgDeprecations } from 'ember-nrg-ui/utils/deprecation-handler';
 
+@AddNrgDeprecations({
+  id: 'nrg-appbar.show-release-notes',
+  test(target) {
+    return target.args.showReleaseNotes !== undefined;
+  },
+  message:
+    '`@showReleaseNotes` is deprecated, please use `@showAppVersion` instead',
+  since: '4.0.0',
+  until: '5.0.0',
+})
 export default class NrgAppbarComponent extends Component {
   @service
   router;
@@ -11,14 +21,6 @@ export default class NrgAppbarComponent extends Component {
   application;
 
   get showAppVersion() {
-    deprecate(
-      'showReleaseNotes is deprecated, please use showAppVersion instead',
-      this.args.showReleaseNotes === undefined,
-      {
-        id: 'nrg-appbar.show-release-notes',
-        until: '5.0.0',
-      }
-    );
     return this.args.showAppVersion !== false;
   }
 
