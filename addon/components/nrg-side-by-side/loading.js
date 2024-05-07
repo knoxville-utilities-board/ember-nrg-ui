72c8e6431e2846d01928277fe7085e95f7328693
TSenter <tyler.senter.427@gmail.com>
2024-05-07 08:36:30 -0400

    feat(side-by-side): Create loading component

---
+++ addon/components/nrg-side-by-side/loading.js
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+import { AddNrgDeprecations } from 'ember-nrg-ui/utils/deprecation-handler';
+
+@AddNrgDeprecations()
+export default class NrgSideBySideLoadingComponent extends Component {}
