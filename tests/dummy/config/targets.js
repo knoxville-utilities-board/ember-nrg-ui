4848f1e335d9a6607c7c291646867825f9935d38
mkaufman94@gmail.com <mkaufman94@gmail.com>
2024-07-05 11:04:28 -0500

    feat: First pass at supported browser configuration

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const browsers = [
-  'Chrome >= 106',
-  'Firefox >= 117',
-  'Safari >= 12',
-  'Edge >= 106',
+  'Chrome >= 96',
+  'Firefox >= 104',
+  'Safari >= 12', // Need more info on this one, get better metrics, 14 should be the latest
+  'Edge >= 96',
 ];
 
 module.exports = {
