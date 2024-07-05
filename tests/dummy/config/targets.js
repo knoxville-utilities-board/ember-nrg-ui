697b898fd09fde17a24ce1c132265508ef7ec5c2
mkaufman94@gmail.com <mkaufman94@gmail.com>
2024-07-05 10:45:53 -0500

    feat: Set supported browsers based on usage data

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const browsers = [
-  'last 1 Chrome versions',
-  'last 1 Firefox versions',
-  'last 1 Safari versions',
+  'Chrome >= 106',
+  'Firefox >= 117',
+  'Safari >= 12',
+  'Edge >= 106',
 ];
 
 module.exports = {
